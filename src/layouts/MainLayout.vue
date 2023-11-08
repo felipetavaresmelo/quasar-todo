93bd495ab4d7130359297abffd2ab04c986fa170
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2023-11-08 00:11:46 -0300

    finalizado

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,45 +2,58 @@
   <q-layout view="lHh Lpr lFf">
     <q-header>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
       </q-toolbar>
       <div class="q-px-lg q-pt-xl q-mb-md ">
         <div class="text-h3">Todo</div>
         <div class="text-subtitle1">{{ todaysDate }}</div>
       </div>
-      <q-img src="src/assets/mountains.jpg" class="header-image absolute-top"/>
+      <q-img src="src/assets/mountains.jpg" class="header-image absolute-top" />
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+    <q-drawer v-model="leftDrawerOpen" show-if-above :width="250" :breakpoint="600">
+      <q-scroll-area style="height: calc(100% - 192px); margin-top: 192px; border-right: 1px solid #ddd">
+        <q-list padding>
+          <q-item to="/" exact clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+            <q-item-section>
+              Todo
+            </q-item-section>
+          </q-item>
+
+          <q-item to="/help" exact clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
+
+            <q-item-section>
+              Help
+            </q-item-section>
+          </q-item>
+
+        </q-list>
+      </q-scroll-area>
+
+      <q-img class="absolute-top" src="/src/assets/mountains.jpg" style="height: 192px">
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img
+              src="https://avatars.githubusercontent.com/u/12265211?s=400&u=27651512c9cbe9b1976526d0ba209d73d5318776&v=4">
+          </q-avatar>
+          <div class="text-weight-bold">Felipe Tavares de Melo</div>
+          <div>@felipetm</div>
+        </div>
+      </q-img>
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+      <keep-alive>
+        <router-view />
+      </keep-alive>
     </q-page-container>
   </q-layout>
 </template>
@@ -103,13 +116,13 @@ export default defineComponent({
     EssentialLink
   },
 
-  setup () {
+  setup() {
     const leftDrawerOpen = ref(false)
 
     return {
       essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
+      toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value
       }
     }
@@ -124,10 +137,10 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-  .header-image {
-    height: 100%;
-    z-index: -1;
-    opacity: 0.2;
-    filter: grayscale(100%);
-  }
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 0.2;
+  filter: grayscale(100%);
+}
 </style>
---
+++ src/pages/Help.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultrices neque nec erat rhoncus, et aliquam elit
+      eleifend. Vestibulum quis nunc cursus, egestas justo rhoncus, tempus ante. Maecenas nisi neque, accumsan vitae
+      lectus ut, semper ultricies nisl. Cras nunc nisl, molestie id vulputate at, molestie sed libero. Ut faucibus dui in
+      mollis scelerisque. Quisque vel metus dictum, mattis nunc ac, tempor magna. Morbi tristique hendrerit vulputate. Sed
+      sollicitudin enim ac placerat suscipit. Ut non enim eleifend, rhoncus dui sit amet, scelerisque nunc. In rutrum
+      tristique sem eget rhoncus. Sed hendrerit mauris viverra mi tristique pellentesque. Praesent varius tincidunt erat
+      eget bibendum. Donec ac malesuada dolor.</p>
+    <p>In vehicula nisl vel ipsum facilisis, vel hendrerit nibh gravida. Quisque eros enim, efficitur at tincidunt vel,
+      rutrum interdum velit. In in lacus sagittis, rutrum libero eu, malesuada dui. Vestibulum efficitur mauris in
+      ultrices blandit. Mauris dolor enim, hendrerit quis bibendum et, fermentum molestie velit. Ut volutpat sagittis
+      tincidunt. In magna ipsum, feugiat id scelerisque in, suscipit non arcu. Etiam nec ullamcorper urna. Maecenas
+      ultrices ligula in mi ornare, ut molestie velit maximus. Nulla ex metus, suscipit non laoreet eu, pulvinar sit amet
+    quam. Duis aliquet leo quam, tempor consequat dui feugiat eu.</p>
+</q-page></template>
+
+<script lang="ts">
+export default {
+
+};
+</script>
